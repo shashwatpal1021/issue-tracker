2918e81a58ce9505e0121fd4294971d5f1d6eaa4
shashwat pal <shashwatpal1021@gmail.com>
2024-04-04 11:48:20 +0530

    refactor the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -9,10 +9,58 @@ import { useSession } from "next-auth/react";
 import { Box, Flex, Container, DropdownMenu, Avatar, Text } from "@radix-ui/themes";
 
 const NavBar = () => {
-  const currentPath = usePathname();
 
+
+  return (
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href={"/"}>
+              <AiFillBug />
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+
+
+const AuthStatus = () => {
   const { status, data: session } = useSession();
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return (<Link className="nav-link" href={'/api/auth/signin'}>Login</Link>);
+  <Box>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <Avatar src={session!.user!.image!} fallback="?"
+          size={'2'}
+          radius="full"
+          className="cursor-pointer"
+        // referrerPolicy="no-referrer"
+        />
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <DropdownMenu.Label>
+          <Text size={'2'}>
+            {session!.user!.email}
+          </Text>
+        </DropdownMenu.Label>
+        <DropdownMenu.Item>
+          <Link href={"/api/auth/signout"}>Log out</Link>
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  </Box>;
+};
 
+const NavLinks = () => {
+  const currentPath = usePathname();
   const links = [
     {
       label: "Dashboard",
@@ -25,62 +73,23 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="border-b mb-5 px-5 py-3">
-      <Container>
-        <Flex justify="between">
-          <Flex align="center" gap="3">
-            <Link href={"/"}>
-              <AiFillBug />
-            </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classnames({
-                      "text-zinc-900": link.href === currentPath,
-                      "text-zinc-500": link.href !== currentPath,
-                      "hover:text-black transition-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          <Box>
-            <Box>
-              {status === "authenticated" && (
-                <DropdownMenu.Root>
-                  <DropdownMenu.Trigger>
-                    <Avatar src={session.user!.image!} fallback="?"
-                      size={'2'}
-                      radius="full"
-                      className="cursor-pointer"
-                      // referrerPolicy="no-referrer"
-                    />
-                  </DropdownMenu.Trigger>
-                  <DropdownMenu.Content>
-                    <DropdownMenu.Label>
-                      <Text size={'2'}>
-                        {session.user!.email}
-                      </Text>
-                    </DropdownMenu.Label>
-                    <DropdownMenu.Item>
-                      <Link href={"/api/auth/signout"}>Log out</Link>
-                    </DropdownMenu.Item>
-                  </DropdownMenu.Content>
-                </DropdownMenu.Root>
-
-              )}
-              {status === "unauthenticated" && (<Link href={'/api/auth/signin'}>Login</Link>)}
-            </Box>
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classnames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
+
 
 export default NavBar;
