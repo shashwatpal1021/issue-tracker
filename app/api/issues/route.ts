40c53487fd00effbba6948ed47412845b99d1425
ShashwatHTS <shashwat.pal@htstechsolutions.com>
2024-04-04 16:27:07 +0530

    Enhance the api to assign issue

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,26 +1,26 @@
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/prisma/client";
-import { issueSchema } from "../../validationSchema";
-import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOption";
+import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { issueSchema } from "../../validationSchema";
 
 export async function POST(request: NextRequest) {
   const session = await getServerSession(authOptions);
   if (!session) {
-    return NextResponse.json({},{status:401})
+    return NextResponse.json({}, { status: 401 });
   }
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), {
-      status:400
-    })
+      status: 400,
+    });
   }
   const newIssue = await prisma.issue.create({
     data: {
-      title: validation.data.title,
-      description: validation.data.description
-    }
-  })
-  return NextResponse.json(newIssue,{status:201})
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
 }
