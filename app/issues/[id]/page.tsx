3b15dfdbe5b407e33f840db2eaa5357654f7e2a4
shashwat pal <shashwatpal1021@gmail.com>
2024-04-04 13:29:31 +0530

    Build the assignee select component

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,6 +6,7 @@ import IssueDetails from './IssueDetails';
 import DeleteIssueButton from './DeleteIssueButton';
 import { getServerSession } from 'next-auth';
 import authOptions from '@/app/auth/authOption';
+import AssigneeSelect from './AssigneeSelect';
 // import delay from 'delay';
 
 interface Props {
@@ -32,7 +33,8 @@ const IssueDetailPage = async ({ params }: Props) => {
         <IssueDetails issue={issue} />
       </Box>
       {session && (<Box>
-        <Flex direction={'column'} gap={'3'}>
+        <Flex direction={'column'} gap={'4'}>
+          <AssigneeSelect/>
           <EditIssueButton issueId={issue.id} />
           <DeleteIssueButton issueId={issue.id} />
         </Flex>
