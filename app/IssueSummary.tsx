269a9536c7bdce0e3832e7ecf9e4f96dd481e7c5
ShashwatHTS <shashwat.pal@htstechsolutions.com>
2024-04-10 14:44:04 +0530

    Build the IssueSummary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,43 @@
+import { Status } from "@prisma/client";
+import { Card, Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  open: number;
+  inPorgress: number;
+  closed: number;
+}
+
+const IssueSummary = ({ open, inPorgress, closed }: Props) => {
+  const containers: {
+    label: string;
+    value: number;
+    status: Status;
+  }[] = [
+    { label: "Open Issues", value: open, status: "OPEN" },
+    { label: "In-progress Issues", value: inPorgress, status: "IN_PROGRESS" },
+    { label: "Closed Issues", value: closed, status: "CLOSED" },
+  ];
+  return (
+    <Flex gap="4">
+      {containers.map((container) => (
+        <Card key={container.label}>
+          <Flex direction="column" gap="1">
+            <Link
+              className="text-sm font-medium"
+              href={`issues/list?status=${container.status}`}
+            >
+              {container.label}
+            </Link>
+            <Text size="5" className="font-bold">
+              {container.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+};
+
+export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
+import prisma from "@/prisma/client";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inPorgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
   return (
     <div className="text-3xl">
       Hello Shashwat
-      <LatestIssues />
+      {/* <LatestIssues /> */}
+      <IssueSummary open={open} inPorgress={inPorgress} closed={closed} />
     </div>
   );
 }
