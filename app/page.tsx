c1e7f3a9819cede09fe5c3ba6b24983585e90b1b
ShashwatHTS <shashwat.pal@htstechsolutions.com>
2024-04-10 11:55:21 +0530

    build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
-    <div className='text-3xl'>
+    <div className="text-3xl">
       Hello Shashwat
+      <Pagination itemCount={100} pageSize={5} currentPage={10} />
     </div>
   );
 }
