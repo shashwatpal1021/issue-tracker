ed8166ba35c927a4b72799589dd897e085d8ccb6
ShashwatHTS <shashwat.pal@htstechsolutions.com>
2024-04-18 15:00:25 +0530

    checking production authentication got failed

--- app/auth/authOption.ts
+++ app/auth/authOption.ts
@@ -7,8 +7,8 @@ const authOptions:NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID! || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET! || "",
     }),
   ],
   session: {
