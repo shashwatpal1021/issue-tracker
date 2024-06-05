5a9d6e8710ee553e027ec26c009d95c7c165ba26
shashwatpal1021 <shashwatpal1021@gmai.com>
2024-06-05 11:22:13 +0530

    fixed list render issue for the first time

--- app/issues/list/optionTable.tsx
+++ app/issues/list/optionTable.tsx
@@ -7,16 +7,15 @@ import IssueTable, { IssueQuery } from "./IssueTable";
 interface Props {
   searchParams: IssueQuery;
   issues: Issue[];
-  data: Issue[];
 }
-const OptionTable = ({ searchParams, issues, data }: Props) => {
+const OptionTable = ({ searchParams, issues }: Props) => {
   const [isIssue, setIsIssue] = useState(false);
   const handleClick = () => setIsIssue((prev) => !prev);
   return (
     <>
       <IssueActions handleClick={handleClick} />
       {isIssue ? (
-        <IssueTable searchParams={searchParams} issues={data} />
+        <IssueTable searchParams={searchParams} issues={issues} />
       ) : (
         <IssueTable searchParams={searchParams} issues={issues} />
       )}
